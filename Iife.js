e1ea395d11e30cfb6e367ba722970ff9cd29d8d1
Ishan Sinha <ishansinha.cool@gmail.com>
2025-06-18 13:49:20 +0530

    added new files and pushed on 18th June

--- Iife.js
+++ Iife.js
@@ -1,4 +1,4 @@
-// Immediately Invoked Fuction Expression (IIFE) : used specially for files contining DB connedction
+// Immediately Invoked Fuction Expression (IIFE) : used specially for files contining DB connection
 (function db_com() {
     console.log(`DB CONNECTED`);
 })(); //this is a named IIFE
